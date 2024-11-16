27dd6bc50d0ff660100442de6483896a72e18cee
OMER YILMAZ <OMER YILMAZ@LAPTOP-GATJMBJU>
2024-11-16 13:26:26 +0300

    Add project files.

---
+++ HW1_Calculator/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace HW1_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator Program");
+            Console.WriteLine();
+            Console.WriteLine("Lütfen bir sayı giriniz:");
+            var number1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Lütfen bir sayı daha giriniz");
+            var number2 = Convert.ToDouble(Console.ReadLine());
+
+            var toplam = number1 + number2;
+            var fark = number1 - number2;
+            var carpim = number1 * number2;
+            var bolum = number1 / number2;
+
+            Console.WriteLine("Sayıların toplamı: " + toplam);
+            Console.WriteLine("Sayıların farkı: " + fark);
+            Console.WriteLine("Sayıların çarpımı: " + carpim);
+            Console.WriteLine("Sayıların bölümü: " + bolum);
+
+        }
+    }
+}
